33f56b8dfc5ee5abb0509b1505f65f19bd29383a
キジトラニャン/お魚咥えなかった世界線 <83628516+kjtrgames@users.noreply.github.com>
2025-09-28 16:04:14 +0000

    ページを追加。

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,38 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'KJTR Games',
+  title: {
+    template: '%s | KJGAMES',
+    default: '作品一覧'
+  },
   description: '作品投稿サイト',
+  openGraph: {
+    title: 'KIJITORAGAMES',
+    description: 'KIJITORAGAMESは、あなたの作品を自由に発信できる場所です。とりあえず見ろ',
+    url: 'https://kjtrgames.github.io',
+    siteName: 'KIJITORA Games',
+    images: [
+      {
+        url: 'https://kjtrgames.github.io/resources/icon.svg',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://kjtrgames.github.io/resources/icon.svg',
+        width: 1800,
+        height: 1600,
+        alt: 'KIJITORAGAMESのアイコン',
+      },
+    ],
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'KIJITORA Games',
+    description: '――― ここは、数多の有志によってゲームが集う場所。―――',
+    images: ['https://kjtrgames.github.io/resources/icon.svg'],
+  },  
 }
 
 export default function RootLayout({ children }) {
@@ -18,24 +48,25 @@ export default function RootLayout({ children }) {
           <label for="^hamburger-io" id="^hamburger"><span></span></label>
           <ul>
             <li><a href="index.html" home>HOME</a></li>
-            <li><a href="/programming">&emsp;プログラミング&emsp;</a>
-              <ul>
-                <li><a href="/programming/js">Java Script</a></li>
-                <li><a href="/programming/scratch">Scratch</a></li>
-              </ul>
-            </li>
             <li><a href="/minecraft">マイクラ</a>
               <ul>
-                <li><a href="/minecraft/building">建築</a></li>
-                <li><a href="/minecraft/redstone">赤石</a></li>
-                <li><a href="/minecraft/rtm">RealTrainMod</a></li>
-                <li><a href="/minecraft/others">その他のMOD</a></li>
+                <li><a href="/minecraft/mods">MOD</a></li>
+                <li><a href="/minecraft/maps">配布マップ</a></li>
+                <li><a href="/minecraft/texturepacks">テクスチャ</a></li>
+                <li><a href="/minecraft/addons">MOD追加パック</a></li>
+                <li><a href="/minecraft/others">その他</a></li>
+              </ul>
+            </li>
+            <li><a href="/games">他のゲーム</a>
+              <ul>
+                <li><a href="/games/js">JSゲーム</a></li>
+                <li><a href="/games/kgscript">\(準備中\)</a></li>
               </ul>
             </li>
             <li><a href="/music">作曲</a>
               <ul>
                 <li><a href="/music/original">オリジナル</a></li>
-                <li><a href="/music/second">二次創作</a></li>
+                <li><a href="/music/second">二次創作･RM</a></li>
                 <li><a href="/music/others">その他</a></li>
               </ul>
             </li>
---
+++ app/minecraft/page.jsx
@@ -0,0 +1,37 @@
+export default function PageMinecraft() {
+  return (
+    <article>
+      <h1>MINECRAFT</h1>
+      <p>
+        マイクラは楽しいですよね、<br />
+        楽しいですよね、<br />
+        楽しいですよね！！！(急にどうした)<br />
+        さて、KIJITORAGAMESでは、
+        <ul>
+          <li>MOD</li>
+          <li>配布マップ</li>
+          <li>テクスチャパック</li>
+        </ul>
+        や、
+        <ul>
+          <li>シェーダーパック</li>
+          <li>RealTrainModの追加パック</li>
+        </ul>
+        なんかも投稿されます。(唐突な正気)
+      </p>
+      <p>
+        そんなわけで、このページに一覧を紹介したら見づらいので、<br />
+        人気の作品やオススメ作品のみ、ここに掲示します。<br />
+        個々のカテゴリーで一覧が見たい方は、上に表示されているメニューから選んでください。<br />
+        トラブルがあった時用リンク<br />
+        <a href="./mods">MOD</a><br />
+        <a href="./maps">配布マップ</a><br />
+        <a href="./texturepacks">テクスチャパック</a><br />
+        <a href="./others">他改造物</a><br />
+      </p>
+      <h2>作品紹介</h2>
+      <div id="works">
+      </div>
+    </article>
+  )
+}
