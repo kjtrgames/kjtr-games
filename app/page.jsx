a92f787076c86730456b96e57a5ffa71acd2079d
キジトラニャン/お魚咥えなかった世界線 <83628516+kjtrgames@users.noreply.github.com>
2025-09-28 17:18:33 +0000

    test仮変更

--- app/page.jsx
+++ app/page.jsx
@@ -1,49 +1,8 @@
-export const metadata = {
-  title: {
-    absolute: true,
-    default: 'KIJITORAGAMES',
-  },
-  description: 'KIJITORAGAMESは、IT愛好会みたいな何かです。うん。とりまアクセスしよう!',
-}
-
 export default function Home() {
-    return (
-        <article>
-        <div id="^main-title">
-          <h1>猫のように、自由気ままに。</h1>
-        </div>
-  
-        <p>
-          どうも皆さんこんにちは、KIJITORA GAMESのキジトラニャンです。<br />
-          KIJITORAGAMESでは主に、マイクラや作曲、ドット絵制作をしています。(現在会員1人w)
-        </p>
-      
-        <h2>KIJITORAGAMESとは?</h2>
-        <p>
-          KIJITORAGAMESとは、私キジトラニャンが個人で開発・運営をしている作品投稿サイトです。<br />
-          マイクラの追加パックや、オリジナルのゲームを制作・公開できます！<br />
-          まだまだ発展途上のサイトですが、これからもよろしくお願いします！！！！！！！！！！！？！？！？？
-        </p>
-    
-        <h2>キジトラニャンの概要</h2>
-        <p>
-          一人称は「猫」。<br />
-          はてなブログの「<a href="https://kjtrgames.hatenablog.jp/">キジトラニャン</a>」、scratchの<a
-            href="https://scratch.mit.edu/users/ryoichiro/">@ryoichiro</a>と同一人物です。一応会長…だよ？
-            /* 
-              この暗号は解いてはいけない...君の名誉のためにも。
-              70, 97, 99, 107, 44, 121, 111 , 117
-            */
-          &#57343;
-        </p>
-        <h2>KIJITORAGAMESのアイコン</h2>
-        <div class="center"><img src="resources/icon.svg" alt="くそッ！画像を見せられてないじゃないか！" title="icon" /></div>
-        <p style="display:inline">
-          わ<nav style="display:inline">　</nav>た<nav style="display:inline">　</nav>し<nav style="display:inline">　</nav>が<nav style="display:inline">　</nav>つ<nav style="display:inline">　</nav>く<nav style="display:inline">　</nav>り<nav style="display:inline">　</nav>ま<nav style="display:inline">　</nav>し<nav style="display:inline">　</nav>た<br /><br />
-          すごいじゃろすごいじゃろ？<br />
-          いちにちでパパッとつくったのだ★☆彡
-        </p>
-        <p className="sover">自慢じゃないっすよ</p>    
-      </article>
-    )
-    }
+  return (
+    <main>
+      <h1>Hello Vercel!</h1>
+      <p>Next.js test page.</p>
+    </main>
+  );
+}
