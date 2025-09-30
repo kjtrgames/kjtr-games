28989f8646f5ca56365fb2339e6d2608df5d2ca5
キジトラニャン/お魚咥えなかった世界線 <83628516+kjtrgames@users.noreply.github.com>
2025-09-30 21:54:50 +0900

    Create not-found.jsx

---
+++ app/not-found.jsx
@@ -0,0 +1,39 @@
+export const metadata = {
+  title: {
+    absolute: "404 NOOOPE",
+  },
+  description: 'サイトが存在しません',
+  openGraph: {
+    title: '4040404040404040404',
+    description: 'これだけは見るな',
+    locale: 'ja_JP',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'NOT FOUND',
+    description: 'これは存在しないページなんよ',
+    images: ['https://kjtrgames.vercel.app/icon.svg'],
+  }, 
+
+}
+
+
+export default function NotFound() {
+  return (
+    <main>
+      <h1>404草草草草乙乙乙乙乙乙乙乙乙wwwwwwwww</h1>
+      <article>
+        <p>
+          やーい底のお兄さん、URL直打ちした〜？<br />
+          だめだよ〜直打ちなんかしたら。こんなふうに惨めに煽られるだけだよ？<br />
+          <br />
+          それともネットにあったURLを踏んだらここにたどり着いたってコトォ〜？<br />
+          あらあらお兄さん、かっわいそ〜ww<br />
+          わざわざ来てくれたのに可哀想ね〜〜〜wwww<br />
+          <br />
+          ...<br />
+          <br />
+          ......ところで、屋上あんだけど焼いてかない?????????????
+        </p>
+      </article>
+}
