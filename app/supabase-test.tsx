1c9c51ec071f2d348fa239f0d8b0557cfd0d7207
キジトラニャン/お魚咥えなかった世界線 <83628516+kjtrgames@users.noreply.github.com>
2025-10-15 08:12:56 +0000

    TSTSTSTS死刑死刑死刑TSTSTS

--- app/supabase-test.tsx
+++ app/supabase-test.tsx
@@ -1,4 +1,4 @@
-import { supabase } from '@/lib/supabaseClient'
+import { supabase } from "@/lib/supabaseClient"
 
 export default async function SupaBaseTestingPage() {
   const { data, error } = await supabase.from('articles').select('*').limit(1)
