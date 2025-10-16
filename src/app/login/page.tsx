ef6f691be6de0e39c637051651cce6e83c5165f0
キジトラニャン/お魚咥えなかった世界線 <83628516+kjtrgames@users.noreply.github.com>
2025-10-16 05:29:24 +0000

    move folders

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { supabase } from '../../lib/supabaseClient'
+import { supabase } from '../../../lib/supabaseClient'
 
 export default function LoginPage() {
   const handleLogin = async () => {
