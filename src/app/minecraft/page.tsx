f8825544f470d47281fca6f8f6b2cc613da893a6
キジトラニャン/お魚咥えなかった世界線 <83628516+kjtrgames@users.noreply.github.com>
2025-10-19 03:24:45 +0000

    Make well改良 pages

--- src/app/minecraft/page.tsx
+++ src/app/minecraft/page.tsx
@@ -31,50 +31,57 @@ export default async function PageMinecraft() {
 
   if (error) {
     articles = <div>Error: {error.message}</div>
-  };
-  if (!data?.length) {
+  } else if (!data?.length) {
     articles = <div>何も投稿されておりません。<br />…は？何も投稿されてないわけ無いだろﾑｯｺﾛｽぞテメー</div>
-  };
+  } else {
+    articles = (
+      <> {
+        data.map(anarticle => (
+          <article className="^an_article" key={anarticle.id}>
+            <h2>{anarticle.title}</h2>
+            <div dangerouslySetInnerHTML={{ __html: anarticle.content }} />
+          </article>
+        ))
+      } </>
+    )
+  }
 
   return (
-    <article>
-      <h1>MINECRAFT</h1>
-      <p>
-        マイクラは楽しいですよね、<br />
-        楽しいですよね、<br />
-        楽しいですよね！！！(急にどうした)<br />
-        さて、KIJITORAGAMESでは、
-        <ul>
-          <li>MOD</li>
-          <li>配布マップ</li>
-          <li>テクスチャパック</li>
-        </ul>
-        や、
-        <ul>
-          <li>シェーダーパック</li>
-          <li>RealTrainModの追加パック</li>
-        </ul>
-        なんかも投稿されます。(唐突な正気)(正気と言えるのだろうかこれは)
-      </p>
-      <p>
-        そんなわけで、このページに一覧を紹介したら見づらいので、<br />
-        人気の作品やオススメ作品のみ、ここに掲示します。<br />
-        個々のカテゴリーで一覧が見たい方は、上に表示されているメニューから選んでください。<br />
-        トラブルがあった時用リンク<br />
-        <a href="./mods">MOD</a><br />
-        <a href="./maps">配布マップ</a><br />
-        <a href="./texturepacks">テクスチャパック</a><br />
-        <a href="./others">他改造物</a><br />
-      </p>
+    <>
+      <article>
+        <h1>MINECRAFT</h1>
+        <p>
+          マイクラは楽しいですよね、<br />
+          楽しいですよね、<br />
+          楽しいですよね！！！(急にどうした)<br />
+          さて、KIJITORAGAMESでは、
+          <ul>
+            <li>MOD</li>
+            <li>配布マップ</li>
+            <li>テクスチャパック</li>
+          </ul>
+          や、
+          <ul>
+            <li>シェーダーパック</li>
+            <li>RealTrainModの追加パック</li>
+          </ul>
+          なんかも投稿されます。(唐突な正気)(正気と言えるのだろうかこれは)
+        </p>
+        <p>
+          そんなわけで、このページに一覧を紹介したら見づらいので、<br />
+          人気の作品やオススメ作品のみ、ここに掲示します。<br />
+          個々のカテゴリーで一覧が見たい方は、上に表示されているメニューから選んでください。<br />
+          トラブルがあった時用リンク<br />
+          <a href="./mods">MOD</a><br />
+          <a href="./maps">配布マップ</a><br />
+          <a href="./texturepacks">テクスチャパック</a><br />
+          <a href="./others">他改造物</a><br />
+        </p>
+      </article>
       <h2>作品紹介</h2>
       <div id="works">
-        {data.map(article => (
-          <div key={article.id}>
-            <h2>{article.title}</h2>
-            <div dangerouslySetInnerHTML={{ __html: article.content }} />
-          </div>
-        ))}
+        {articles}
       </div>
-    </article>
+    </>
   )
 }
