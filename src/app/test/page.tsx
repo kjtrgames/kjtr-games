b20cd00e985eb1f438d7bfa899b678cb01f75ff4
キジトラニャン/お魚咥えなかった世界線 <83628516+kjtrgames@users.noreply.github.com>
2025-10-21 10:34:31 +0000

    Fix test/page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 import { supabase } from "@/lib/supabaseClient";
 
-export default async function SupaBaseTestingPage() {
+export default async function TestPage() {
   const { data, error } = await supabase.from('articles').select('*').limit(1)
 
   console.log('data:', data)
@@ -12,18 +10,20 @@ export default async function SupaBaseTestingPage() {
     <>
       <h1>色々テストするためのページ</h1>
       <main>
-        <label>
-          <input type="radio" name="menu" />
-          メニュー1
-        </label>
-        <label>
-          <input type="radio" name="menu" />
-          メニュー2
-        </label>
-        <label>
-          <input type="radio" name="menu" />
-          メニュー3
-        </label>
+        <article>
+          <label>
+            <input type="radio" name="menu" />
+            メニュー1
+          </label>
+          <label>
+            <input type="radio" name="menu" />
+            メニュー2
+          </label>
+          <label>
+            <input type="radio" name="menu" />
+            メニュー3
+          </label>
+        </article>
       </main>
     </>
 )}
