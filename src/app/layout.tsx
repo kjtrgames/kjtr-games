46a8d7298720cda06bdb4f9b7f81adf5b58aab3e
キジトラニャン/お魚咥えなかった世界線 <83628516+kjtrgames@users.noreply.github.com>
2025-10-24 07:24:18 +0000

    Fix not using <header> in navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,79 +37,79 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <h1>お詫び</h1>
         <p>このサイトは、JavaScriptが必須となっております。ご迷惑をおかけします。</p>
       </noscript>  
-      <header>
-        <nav>
-          <input type="checkbox" id="^hamburger-io" />
-          <label htmlFor="^hamburger-io" id="^hamburger"><span></span></label>
-          <ul id="^standard-menu">
-            <li><a href="/" className="home"><img src="/home.svg" /></a></li>
-            <li><a href="/minecraft">マイクラ</a>
-              <ul>
-                <li><a href="/minecraft/mods">MOD</a></li>
-                <li><a href="/minecraft/maps">配布マップ</a></li>
-                <li><a href="/minecraft/texturepacks">テクスチャ</a></li>
-                <li><a href="/minecraft/addons">MOD追加パック</a></li>
-                <li><a href="/minecraft/others">その他</a></li>
-              </ul>
-            </li>
-            <li><a href="/games">他のゲーム</a>
-              <ul>
-                <li><a href="/games/js">JSゲーム</a></li>
-                <li><a href="/games/kgscript">\(準備中\)</a></li>
-              </ul>
-            </li>
-            <li><a href="/music">作曲</a>
-              <ul>
-                <li><a href="/music/original">オリジナル</a></li>
-                <li><a href="/music/second">二次創作･RM</a></li>
-                <li><a href="/music/others">その他</a></li>
-              </ul>
-            </li>
-            <li><a href="/pictures">絵</a>
-              <ul>
-                <li><a href="/pictures/dots">ドット絵</a></li>
-                <li><a href="/pictures/others">その他の絵</a></li>
-              </ul>
-            </li>
-            <li><a href="https://forms.gle/VAdJjueZNdRPJi9aA">制作依頼</a></li>
-          </ul>
+      
+      <nav>
+        <input type="checkbox" id="^hamburger-io" />
+        <label htmlFor="^hamburger-io" id="^hamburger"><span></span></label>
+        <ul id="^standard-menu">
+          <li><a href="/" className="home"><img src="/home.svg" /></a></li>
+          <li><a href="/minecraft">マイクラ</a>
+            <ul>
+              <li><a href="/minecraft/mods">MOD</a></li>
+              <li><a href="/minecraft/maps">配布マップ</a></li>
+              <li><a href="/minecraft/texturepacks">テクスチャ</a></li>
+              <li><a href="/minecraft/addons">MOD追加パック</a></li>
+              <li><a href="/minecraft/others">その他</a></li>
+            </ul>
+          </li>
+          <li><a href="/games">他のゲーム</a>
+            <ul>
+              <li><a href="/games/js">JSゲーム</a></li>
+              <li><a href="/games/kgscript">\(準備中\)</a></li>
+            </ul>
+          </li>
+          <li><a href="/music">作曲</a>
+            <ul>
+              <li><a href="/music/original">オリジナル</a></li>
+              <li><a href="/music/second">二次創作･RM</a></li>
+              <li><a href="/music/others">その他</a></li>
+            </ul>
+          </li>
+          <li><a href="/pictures">絵</a>
+            <ul>
+              <li><a href="/pictures/dots">ドット絵</a></li>
+              <li><a href="/pictures/others">その他の絵</a></li>
+            </ul>
+          </li>
+          <li><a href="https://forms.gle/VAdJjueZNdRPJi9aA">制作依頼</a></li>
+        </ul>
 
-          <span id="^hamburger-menu">
+        <span id="^hamburger-menu">
+          <input type="radio" name="^header-nav-index" />
+          <a href="/" className="home"><img src="/home.svg" alt="home" /></a>
+          <label>
             <input type="radio" name="^header-nav-index" />
-            <a href="/" className="home"><img src="/home.svg" alt="home" /></a>
-            <label>
-              <input type="radio" name="^header-nav-index" />
-              <a href="/minecraft">マイクラ</a>
-              <ul>
-                <li><a href="/minecraft/mods">MOD</a></li>
-                <li><a href="/minecraft/maps">配布マップ</a></li>
-                <li><a href="/minecraft/texturepacks">テクスチャ</a></li>
-                <li><a href="/minecraft/addons">MOD追加パック</a></li>
-                <li><a href="/minecraft/others">その他</a></li>
-              </ul>
-            </label>
-            <label>
-              <input type="radio" name="^header-nav-index" />
-              <a href="/music">作曲</a>
-              <ul>
-                <li><a href="/music/original">オリジナル</a></li>
-                <li><a href="/music/second">二次創作･RM</a></li>
-                <li><a href="/music/others">その他</a></li>
-              </ul>
-            </label>
-            <label>
-              <input type="radio" name="^header-nav-index" />
-              <a href="/pictures">絵</a>
-              <ul>
-                <li><a href="/pictures/dots">ドット絵</a></li>
-                <li><a href="/pictures/others">その他の絵</a></li>
-              </ul>
-            </label>
-            <a href="https://forms.gle/VAdJjueZNdRPJi9aA">制作依頼</a>
-          </span>
+            <a href="/minecraft">マイクラ</a>
+            <ul>
+              <li><a href="/minecraft/mods">MOD</a></li>
+              <li><a href="/minecraft/maps">配布マップ</a></li>
+              <li><a href="/minecraft/texturepacks">テクスチャ</a></li>
+              <li><a href="/minecraft/addons">MOD追加パック</a></li>
+              <li><a href="/minecraft/others">その他</a></li>
+            </ul>
+          </label>
+          <label>
+            <input type="radio" name="^header-nav-index" />
+            <a href="/music">作曲</a>
+            <ul>
+              <li><a href="/music/original">オリジナル</a></li>
+              <li><a href="/music/second">二次創作･RM</a></li>
+              <li><a href="/music/others">その他</a></li>
+            </ul>
+          </label>
+          <label>
+            <input type="radio" name="^header-nav-index" />
+            <a href="/pictures">絵</a>
+            <ul>
+              <li><a href="/pictures/dots">ドット絵</a></li>
+              <li><a href="/pictures/others">その他の絵</a></li>
+            </ul>
+          </label>
+          <a href="https://forms.gle/VAdJjueZNdRPJi9aA">制作依頼</a>
+        </span>
 
-        </nav>
-      </header>
+      </nav>
+      
       <main>
         {children}
       </main>
